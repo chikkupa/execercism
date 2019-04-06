bb226e0debc786f23c793c58535c6dc2b82d7fbc
chikkupa <chikku@hashin.in>
2019-04-06 12:17:53 +0530

    scrabble-score

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,6 +1,8 @@
 package scrabble
 
-var scores = map[int]int{
+import "strings"
+
+var scores = map[rune]int{
 	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
 	'D': 2, 'G': 2,
 	'B': 3, 'C': 3, 'M': 3, 'P': 3,
@@ -13,14 +15,9 @@ var scores = map[int]int{
 // Score to find the scrabble score of a string
 func Score(value string) int {
 	var score int
-	for i := 0; i < len(value); i++ {
-		if value[i] >= 65 && value[i] <= 90 {
-			index := int(value[i])
-			score += scores[index]
-		} else if value[i] >= 97 && value[i] <= 122 {
-			index := int(value[i])
-			score += scores[index-32]
-		}
+
+	for _, char := range strings.ToUpper(value) {
+		score += scores[char]
 	}
 
 	return score
