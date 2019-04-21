345208a0b615ff10f84d7500d22c51677341e3ec
chikkupa <chikku@hashin.in>
2019-04-21 22:00:56 +0530

    list-ops

--- go/list-ops/listops.go
+++ go/list-ops/listops.go
@@ -1,25 +1,29 @@
 package listops
 
+// IntList custom type for list of integers
 type IntList []int
 type binFunc func(x, y int) int
 type predFunc func(n int) bool
 type unaryFunc func(x int) int
 
-func (list IntList)Foldl(fn binFunc, initial int) int {
+// Foldl foldl function
+func (list IntList) Foldl(fn binFunc, initial int) int {
 	for _, item := range list {
 		initial = fn(initial, item)
 	}
 	return initial
 }
 
-func (list IntList)Foldr(fn binFunc, initial int) int {
+// Foldr foldr function
+func (list IntList) Foldr(fn binFunc, initial int) int {
 	for i := len(list) - 1; i >= 0; i-- {
-		initial = fn(initial, list[i])
+		initial = fn(list[i], initial)
 	}
 	return initial
 }
 
-func (list IntList)Filter(fn predFunc) IntList {
+// Filter filter list satisfies the predicate function
+func (list IntList) Filter(fn predFunc) IntList {
 	filterList := []int{}
 
 	for _, item := range list {
@@ -31,31 +35,35 @@ func (list IntList)Filter(fn predFunc) IntList {
 	return filterList
 }
 
-func (list IntList)Length() int {
+// Length fuction to find the length of a list
+func (list IntList) Length() int {
 	var length int
 
-	for _ = range list {
+	for range list {
 		length++
 	}
 
 	return length
 }
 
-func (list IntList)Map(fn unaryFunc) IntList {
+// Map equivalent to map function in functional programming
+func (list IntList) Map(fn unaryFunc) IntList {
 	for i, item := range list {
 		list[i] = fn(item)
 	}
 	return list
 }
 
-func (list IntList)Reverse() IntList {
-	for i := 0; i < (len(list) + 1) / 2; i++ {
-		list[i], list[len(list) - (i + 1)] = list[len(list) - (i + 1)], list[i]
+// Reverse reverse the items in the list
+func (list IntList) Reverse() IntList {
+	for i := 0; i < (len(list)+1)/2; i++ {
+		list[i], list[len(list)-(i+1)] = list[len(list)-(i+1)], list[i]
 	}
 	return list
 }
 
-func (list IntList)Append(newList IntList) IntList {
+// Append append a new item to the list
+func (list IntList) Append(newList IntList) IntList {
 	for _, item := range newList {
 		list = append(list, item)
 	}
@@ -63,7 +71,8 @@ func (list IntList)Append(newList IntList) IntList {
 	return list
 }
 
-func (list IntList)Concat(newLists []IntList) IntList {
+// Concat concatenate a list of list to another list
+func (list IntList) Concat(newLists []IntList) IntList {
 	for _, newList := range newLists {
 		for _, item := range newList {
 			list = append(list, item)
