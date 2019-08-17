d6526ecb22fe2625aeb3fce4a5a0f36aa1aa4c06
chikkupa <chikku@hashin.in>
2019-08-17 14:52:40 +0530

    Secret Handshake

--- go/secret-handshake/secret_handshake.go
+++ go/secret-handshake/secret_handshake.go
@@ -1,47 +1,27 @@
 package secret
 
-import (
-	"fmt"
-	"strconv"
-)
+var handShakesArray = []string{
+	"wink",
+	"double blink",
+	"close your eyes",
+	"jump",
+}
 
+// Handshake function returns the array of secret handshakes
 func Handshake(code uint) []string {
-	var binary string
-	handShakes := []string{
-		"wink",
-		"double blink",
-		"close your eyes",
-		"jump",
-		"",
-	}
+	var handshakes []string
 
-	var handshake []string
-
-	if code < 15 {
-		if code % 2 == 1 { handshake = append(handshake, handShakes[0]) }
-	}
-	for(code > 0){
-		binary = strconv.Itoa(int(code % 2)) + binary
+	for i := 0; code > 0; i++ {
+		if code%2 == 1 && i < 4 {
+			handshakes = append(handshakes, handShakesArray[i])
+		} else if code%2 == 1 {
+			length := len(handshakes)
+			for i := 0; i < length/2; i++ {
+				handshakes[i], handshakes[length-i-1] = handshakes[length-i-1], handshakes[i]
+			}
+		}
 		code /= 2
 	}
 
-	fmt.Println(binary)
-
-	if(len(binary) > 4){
-		binary = binary[1:]
-		strlen := len(binary)
-		for index, char := range binary {
-			if(char == '1'){
-				handshake = append(handshake, handShakes[strlen - index - 1])
-			}
-		}
-	} else {
-		for index, char := range binary {
-			if(char == '1'){
-				handshake = append(handshake, handShakes[index])
-			}
-		}
-	}
-
-	return handshake
+	return handshakes
 }
