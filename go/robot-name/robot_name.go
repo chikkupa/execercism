50c7ecfcaa73059b88f2f5bda1a101cc9a349372
chikkupa <chikku@hashin.in>
2019-09-18 21:47:49 +0530

    go/robot-name/.

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -7,49 +7,47 @@ import (
 )
 
 // Robot Structure to save robot name
-type Robot string
+type Robot struct{ RobotName string }
 
-var record = make(map[Robot]bool)
+var record = make(map[string]bool)
 
 // Name Generate a rondom name for robot and
 // return the name
 func (r *Robot) Name() (string, error) {
-	if *r == "" {
+	if r.RobotName == "" {
 		if len(record) > (26*26*10*10*10 - 1) {
 			return "", errors.New("Namespace exhausted")
 		}
 
 		for true {
 			r.generateName()
-			if !record[*r] {
+			if !record[r.RobotName] {
 				break
 			}
 		}
 
-		record[*r] = true
-
-		return string(*r), nil
+		record[r.RobotName] = true
 	}
-	return string(*r), nil
+	return r.RobotName, nil
 }
 
 func (r *Robot) generateName() {
 	s1, s2 := string(rand.Intn(26)+'A'), string(rand.Intn(26)+'A')
 	number := rand.Intn(1000)
 
-	*r = Robot(fmt.Sprintf("%s%s%3d", s1, s2, number))
+	r.RobotName = fmt.Sprintf("%s%s%3d", s1, s2, number)
 }
 
 // Reset Delete the name of the robot and regenerate its name
 func (r *Robot) Reset() (string, error) {
 	for true {
 		r.generateName()
-		if !record[*r] {
+		if !record[r.RobotName] {
 			break
 		}
 	}
 
-	record[*r] = true
+	record[r.RobotName] = true
 
-	return string(*r), nil
+	return r.RobotName, nil
 }
